553f6b8669c3402b0d106d1fbd399971a59ec022
morde <mordechai.hadad01@gmail.com>
2024-07-23 14:49:13 +0300

    add more info to list command

--- src/handlers/list_handler.rs
+++ src/handlers/list_handler.rs
@@ -24,13 +24,11 @@ pub async fn start() -> Result<()> {
     );
     let total_paths_string = format!("Total Paths: {}", total_paths);
     println!(
-        "║ {total_paths_string}{}║",
-        " ".repeat(TABLE_WIDTH + header_length - total_paths_string.len() + 4)
+        "║ {total_paths_string:<87}║"
     );
     let last_backup_string = format!("Last Backup: {}", format_last_backup(last_backup));
     println!(
-        "║ {last_backup_string}{}║",
-        " ".repeat(TABLE_WIDTH + header_length - last_backup_string.len() + 4)
+        "║ {last_backup_string:<87}║",
     );
     println!(
         "╠{}╣", "═".repeat(TABLE_WIDTH + header_length + 5)
@@ -43,7 +41,13 @@ pub async fn start() -> Result<()> {
             path.to_str().unwrap().to_string()
         };
 
-        println!("║ {}. {}{}║", index + 1, truncated_path, " ".repeat(TABLE_WIDTH + header_length - truncated_path.len() + 1));
+        println!("║ {}. {truncated_path:<84}║", index + 1);
+        println!(
+            "║    └─ Hash: {:<74} ║",
+            entry.hash
+        );
+        println!("║    └─ Size: {:<74} ║", humanize_bytes(entry.metadata.size));
+        println!("║    └─ Last Modified: {:<65} ║", entry.metadata.last_modified.format("%Y-%m-%d %H:%M:%S"));
         println!(
             "║    └─ Last Backup: {:<67} ║",
             format_last_backup(entry.last_backup)
@@ -66,4 +70,17 @@ fn format_last_backup(last_backup: Option<DateTime<Utc>>) -> String {
         Some(date_time) => date_time.format("%Y-%m-%d %H:%M:%S").to_string(),
         None => "None".to_string(),
     }
+}
+
+fn humanize_bytes(bytes: u64) -> String {
+    let sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    if bytes == 0 {
+        return "0 Bytes".to_string();
+    }
+    let i = (bytes as f64).log(1024.0).floor() as i32;
+    if i == 0 {
+        return format!("{} {}", bytes, sizes[i as usize]);
+    }
+    let size = (bytes as f64) / 1024.0_f64.powi(i);
+    format!("{:.3} {}", size, sizes[i as usize])
 }
